8e5ca4f54862fccf78f2bd80f50a1fd869ea1a87
Diamax <Diamax@ASUS>
2023-06-05 03:36:05 +0300

    Добавьте файлы проекта.

---
+++ EduApp/LoginForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace EduApp
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginField = new System.Windows.Forms.TextBox();
+            this.passField = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.HidePass = new System.Windows.Forms.PictureBox();
+            this.ShowPass = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.HidePass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ShowPass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.HidePass);
+            this.panel1.Controls.Add(this.ShowPass);
+            this.panel1.Controls.Add(this.loginButton);
+            this.panel1.Controls.Add(this.passField);
+            this.panel1.Controls.Add(this.loginField);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(47, 72);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(399, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(226)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(397, 92);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Авторизация";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // loginField
+            // 
+            this.loginField.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.loginField.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.loginField.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginField.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(226)))), ((int)(((byte)(255)))));
+            this.loginField.Location = new System.Drawing.Point(121, 122);
+            this.loginField.Multiline = true;
+            this.loginField.Name = "loginField";
+            this.loginField.Size = new System.Drawing.Size(220, 64);
+            this.loginField.TabIndex = 3;
+            this.loginField.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // passField
+            // 
+            this.passField.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.passField.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passField.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passField.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(226)))), ((int)(((byte)(255)))));
+            this.passField.Location = new System.Drawing.Point(121, 224);
+            this.passField.Name = "passField";
+            this.passField.Size = new System.Drawing.Size(220, 45);
+            this.passField.TabIndex = 4;
+            this.passField.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.passField.UseSystemPasswordChar = true;
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(226)))), ((int)(((byte)(255)))));
+            this.loginButton.FlatAppearance.BorderSize = 2;
+            this.loginButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.loginButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(226)))), ((int)(((byte)(255)))));
+            this.loginButton.Location = new System.Drawing.Point(87, 329);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(220, 48);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.Text = "Вход";
+            this.loginButton.UseVisualStyleBackColor = false;
+            // 
+            // HidePass
+            // 
+            this.HidePass.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.HidePass.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.HidePass.Image = global::EduApp.Properties.Resources.hidePass;
+            this.HidePass.Location = new System.Drawing.Point(347, 239);
+            this.HidePass.Name = "HidePass";
+            this.HidePass.Size = new System.Drawing.Size(40, 40);
+            this.HidePass.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.HidePass.TabIndex = 8;
+            this.HidePass.TabStop = false;
+            this.HidePass.Visible = false;
+            this.HidePass.Click += new System.EventHandler(this.HidePass_Click);
+            // 
+            // ShowPass
+            // 
+            this.ShowPass.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ShowPass.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ShowPass.Image = global::EduApp.Properties.Resources.showPass;
+            this.ShowPass.Location = new System.Drawing.Point(347, 239);
+            this.ShowPass.Name = "ShowPass";
+            this.ShowPass.Size = new System.Drawing.Size(40, 40);
+            this.ShowPass.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ShowPass.TabIndex = 7;
+            this.ShowPass.TabStop = false;
+            this.ShowPass.Click += new System.EventHandler(this.ShowPass_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.pictureBox2.Image = global::EduApp.Properties.Resources._lock;
+            this.pictureBox2.Location = new System.Drawing.Point(51, 224);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Padding = new System.Windows.Forms.Padding(3);
+            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.pictureBox1.Image = global::EduApp.Properties.Resources.user;
+            this.pictureBox1.Location = new System.Drawing.Point(51, 122);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(3);
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.ClientSize = new System.Drawing.Size(495, 560);
+            this.Controls.Add(this.panel1);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Вход/регистрация";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.HidePass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ShowPass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox passField;
+        private System.Windows.Forms.TextBox loginField;
+        private System.Windows.Forms.PictureBox ShowPass;
+        private System.Windows.Forms.PictureBox HidePass;
+    }
+}
+
---
+++ EduApp/LoginForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EduApp
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+            this.passField.AutoSize = false;
+            this.passField.Size = new Size(this.passField.Width,this.loginField.Height);
+        }
+
+        private void ShowPass_Click(object sender, EventArgs e)
+        {
+            passField.UseSystemPasswordChar= false;
+            ShowPass.Visible = false;
+            HidePass.Visible = true;
+        }
+
+        private void HidePass_Click(object sender, EventArgs e)
+        {
+            passField.UseSystemPasswordChar = true;
+            ShowPass.Visible = true;
+            HidePass.Visible = false;
+        }
+    }
+}
